dac7d8923c81fad50621677ed6408cdc62353157
vimal <selvamvimaldz1@gmail.com>
2024-02-21 00:25:57 +0530

    fix:bug

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
+let arr = [
+    { plot: 5, cost: 1500 },
+    { plot: 4, cost: 1000 },
+    { plot: 10, cost: 3000 }
+];
 
-const findmaxProfit = (input) => {
-    let theater = 5;
-    let pub = 4;
-    let comm = 10;
-    let theaterCost = 1500;
-    let pubCost = 1000;
-    let comCost = 3000;
-    let maxprofit =0;
-    if (theater < input) {
-        let bal = input - theater;
-        let profit = bal * theaterCost;
-        
-        
-        if (bal > theater) {
-            let val = findmaxProfit(bal);
+const findmaxProfit = (plot, cost, input) => {
+    let profit = 0;
+    if (plot < input) {
+        let bal = input - plot;
+        profit = bal * cost;
+        if (bal > plot) {
+            let val = findmaxProfit(plot, cost, bal);
             profit += val
-        }
-        if (maxprofit < profit) {
-            maxprofit = profit
-        }
-    } if (pub < input) {
-        let bal = input - pub;
-        let profit = bal * pubCost
-        if (bal > pub) {
-            let val = findmaxProfit(bal);
-            profit += val
-        }
-        if (maxprofit < profit) {
-            maxprofit = profit
-        }
-    } if (comm < input) {
-        let bal = input - comm;
-        let profit = bal * comCost
-        if (bal > comm) {
-            let val = findmaxProfit(bal);
-            profit += val
-        }
-        if (maxprofit < profit) {
-            maxprofit = profit
         }
     }
-    return maxprofit
+    return profit
 }
-let input = 13;
-console.log('$'+findmaxProfit(input));
+let max = 0;
+arr.forEach((e) => {
+    let data = findmaxProfit(e.plot, e.cost, 13);
+    if (max < data) {
+        max = data;
+    }
+})
+console.log('$ '+max);
